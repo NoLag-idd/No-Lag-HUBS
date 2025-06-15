f8e4372c4eab6bf524fa36e218989ad22b82b3b3
NoLag-idd <richardpuno51@gmail.com>
2025-06-15 13:50:16 +0800

    Update loaderV1.lua

--- loader/loaderV1.lua
+++ loader/loaderV1.lua
@@ -1,5 +1,7 @@
-if identifyexecutor and identifyexecutor():lower():find("delta") then
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-idd/No-Lag-HUBS/refs/heads/main/loader/deltaloader"))()
+local executor = identifyexecutor and identifyexecutor() or "Unknown"
+
+if string.lower(executor):find("delta") then
+    game.Players.LocalPlayer:Kick("This is not working in Delta Executor.")
 else
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-idd/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
 end
